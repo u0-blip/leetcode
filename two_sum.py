4e909a01e0961bcb2390ef8943572da3be96cceb
Duona Chen <dche145@student.monash.edu>
2020-05-31 10:17:37 +1000

    two sum

---
+++ two_sum.py
@@ -0,0 +1,19 @@
+nums = [3,2,3]
+
+target = 6
+from itertools import islice
+import numpy as np
+
+args = np.argsort(nums)
+nums = np.sort(nums)
+#brute force
+for i in range(len(nums)):
+    value_to_look = target - nums[i]
+    a = np.searchsorted(nums[i+1:], value_to_look)
+    if a+i+1 >= len(nums):
+        if nums[a+i] == value_to_look:
+            print(sorted([args[i], args[a+i+1]]))
+    else:
+        if nums[a+i+1] == value_to_look:
+            print(sorted([args[i], args[a+i+1]]))
+        
